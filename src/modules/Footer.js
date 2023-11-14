2ab4cd1caff1de6fdc4903836358de15503a2898
mcasper78 <121960293+mcasper78@users.noreply.github.com>
2023-11-14 09:03:30 -0500

    Update Footer.js
    adding in first version of footer, testing out tailwind

--- src/modules/Footer.js
+++ src/modules/Footer.js
@@ -1,14 +1,15 @@
 const Footer = () => {
   return (
     <>
-  <div className="container mx-auto py-16 px-5	 text-center">
-  <h1 className="text-2xl md:text-5xl mb-3">Hope your holiday sweater has you in stitches</h1>
-<p>But in all seriousness, it’s the season of giving! That’s why for every sweater you submit
+  <div className="container mx-auto py-16 px-5	text-center">
+  <h1 className="text-2xl md:text-5xl max-w-2xl  mx-auto mb-3">Hope your holiday sweater has you in stitches</h1>
+ 
+<div class="max-w-2xl mx-auto"><p>But in all seriousness, it’s the season of giving! That’s why for every sweater you submit
 The Food Group will donate 10 meals to feeding America. 
 
-
+<br></br>
 (insert progress bar here)  
-</p></div></>
+</p></div> </div></>
   )
 }
 export default Footer;
