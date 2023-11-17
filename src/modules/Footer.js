aded3b0d6f9d8e86c9678ee574873288d6f83bfb
mcasper78 <121960293+mcasper78@users.noreply.github.com>
2023-11-17 10:15:40 -0500

    add progressbar

--- src/modules/Footer.js
+++ src/modules/Footer.js
@@ -1,35 +1,30 @@
 import React from "react";
-import ProgressBar from "@ramonak/react-progress-bar";
 
-const Example = () => {
-  return <ProgressBar />;
-};
+import MealsProgressBar from "./MealsProgressBar.js";
+
+
 
 const Footer = () => {
   return (
     <>
-      <div className="h-full mx-auto py-16 px-5	text-center bg-white">
-        <h2 className="text-2xl md:text-5xl max-w-2xl  mx-auto mb-3">Hope your holiday sweater has you in stitches</h2>
+      <div className="h-full mx-auto py-20 px-5	text-center bg-white min-h-screen"
+        style={{
+          // add backgroundImage from public folder:
+          backgroundImage: `url(${process.env.PUBLIC_URL + '/images/back-stitched.jpg'
+            })`,
+          backgroundRepeat: "repeat",
+
+          backgroundPosition: "top center",
+        }}>
+        <h2 className="text-2xl md:text-5xl max-w-2xl  mx-auto mb-3"
+          style={{ color: "#859123" }}>
+          Hope your holiday sweater has you in stitches</h2>
 
         <div class="max-w-2xl mx-auto"><p>But in all seriousness, it’s the season of giving! That’s why for every sweater you submit The Food Group will donate 10 meals to feeding America.</p>
-          <ProgressBar
-            completed={800}
-            height="50px"
-            labelColor="#ffffff"
-            padding="0px"
-            baseBgColor="#2A3844"
-            bgColor="#859123"
-            initCompletedOnAnimation="0"
-            labelSize="23px"
-            transitionDuration="5s"
-            animateOnRender="true"
-            className="mt-4"
-            maxCompleted={1000}
-            customLabel=" 800 meals"
+          <p className="mb-4 mt-4 font-bold">CHECK OUT OUR PROGRESS</p>
+          <MealsProgressBar />
 
-          />
-
-        </div> 
+        </div>
       </div>
 
       <div className="container mx-auto py-16 px-5 bg-slate-300	text-center">
