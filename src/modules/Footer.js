26eadabdb664d634be94751f70292b4550100875
mcasper78 <121960293+mcasper78@users.noreply.github.com>
2023-11-29 12:34:46 -0500

    Update Footer.js

--- src/modules/Footer.js
+++ src/modules/Footer.js
@@ -24,10 +24,9 @@ const Footer = () => {
         <div class="siteTitle p-8">
         <h2 className="text-2xl md:text-5xl leading-6 max-w-2xl text-amber-300 mt-5  mx-auto mb-3">
          We Hope Your Holiday Sweater Has You in Stitches</h2>
-
         <div className="max-w-xl mx-auto md:text-lg">
           <h2 className="text-white">But in all seriousness, it’s the season of giving! That’s why for every sweater you submit The Food Group will donate 10 meals to Feeding America.</h2>
-          <p className="mb-4 mt-4 text-white font-bold" style={{ color: "#324564" }}>MEALS DONATED TO DATE:</p>
+          <p className="mb-4 mt-4 text-white font-bold" style={{ color: "#869123" }}>MEALS DONATED TO DATE:</p>
         </div>
 
         <div className =" max-w-4xl mx-auto " >
