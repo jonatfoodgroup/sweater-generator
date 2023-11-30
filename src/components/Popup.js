c83bf507d2e7e2b7bbd794e3ac9a0fc60e7fbdf2
Tourné Torres <tournetorres@gmail.com>
2023-11-30 13:03:49 -0600

    add popup

---
+++ src/components/Popup.js
@@ -0,0 +1,45 @@
+// import React from 'react';
+// import Popup from 'reactjs-popup';
+// import 'reactjs-popup/dist/index.css';
+
+// export default () => (
+//     <Popup trigger={<button className="button"> Trigger </button>}
+//       nested>
+//          {close => (
+//       <div className="modal">
+//         <button className="close" onClick={close}>
+//           &times;
+//         </button>
+//         <div className="header"> Modal Title </div>
+//         <div className="content">
+//           {' '}
+//           Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, a nostrum.
+//           Dolorem, repellat quidem ut, minima sint vel eveniet quibusdam voluptates
+//           delectus doloremque, explicabo tempore dicta adipisci fugit amet dignissimos?
+//           <br />
+//           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sit
+//           commodi beatae optio voluptatum sed eius cumque, delectus saepe repudiandae
+//           explicabo nemo nam libero ad, doloribus, voluptas rem alias. Vitae?
+//         </div>
+//         <div className="actions">
+//           <Popup
+//             trigger={<button className="button"> Trigger </button>}
+//             position="top center"
+//             nested
+//           >
+// <div>Your sweater is now hanging in the TFG closet and we thank you for contributing meals to Feeding America.</div>
+//           </Popup>
+//           <button
+//             className="button"
+//             onClick={() => {
+//               console.log('modal closed ');
+//               close();
+//             }}
+//           >
+//             close modal
+//           </button>
+//         </div>
+//       </div>
+//     )}
+// </Popup>
+// )
