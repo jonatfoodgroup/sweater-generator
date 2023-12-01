d9fc71b3290b8e7922c4f57dee5d4744841978f7
Jon Senterfitt <jon.senterfitt@thefoodgroup.com>
2023-12-01 13:39:43 -0500

    Added route for single image
    Added route for single image viewer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
 // src/App.js
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./components/Header";
 import Masthead from "./modules/Masthead";
 import Gallery from "./modules/Gallery";
 import Footer from "./modules/Footer";
+import { useParams } from "react-router-dom";
+import { db } from "./firebase/database";
+import {  ref, get } from "firebase/database";
 
 function App() {
-  // Define an array of image URLs
-const imageUrls = [
-  "https://source.unsplash.com/random/400x300?hotdog",
-  "https://source.unsplash.com/random/400x300?sweater",
-  "https://source.unsplash.com/random/400x300?food",
-  "https://source.unsplash.com/random/400x300?hoodie",
-];
+  const { id } = useParams();
+  const [singleImage, setSingleImage] = useState(null);
   const [selectedImage, setSelectedImage] = useState(null);
+
+  useEffect(() => {
+    if (id) {
+    //  Check the database for the image with the id
+    //  If the image exists, set it as the selected image
+    //  If the image does not exist, redirect to the homepage
+
+    const docRef = ref(db, "gallery/" + id);
+    get(docRef)
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          console.log(snapshot.val());
+          setSingleImage(snapshot.val());
+        } else {
+          console.log("No such document!");
+        }
+      })
+      .catch((error) => {
+        console.log("Error getting document:", error);
+      });
+    
+  }
+  }, [id]);
+  // Define an array of image URLs
+  const imageUrls = [
+    "https://source.unsplash.com/random/400x300?hotdog",
+    "https://source.unsplash.com/random/400x300?sweater",
+    "https://source.unsplash.com/random/400x300?food",
+    "https://source.unsplash.com/random/400x300?hoodie",
+  ];
+  
+  if (singleImage) {
+    return (
+      <div className="App text-center justify-center">
+        <SingleImage image={singleImage} />
+      </div>
+    );
+  }
   return (
     <div className="App">
       <Header />
@@ -42,6 +78,14 @@ const imageUrls = [
         <Gallery images={imageUrls} />
         </div>
         <Footer />
+    </div>
+  );
+}
+
+const SingleImage = ({ image }) => {
+  return (
+    <div className="single-image text-center">
+      <img src={image} alt="" className="w-1/2 m-auto" />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/images/:id" element={<App />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
