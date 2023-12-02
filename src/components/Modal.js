015219e7b182736b7a973f02b41eaaa81415d25c
Tourné Torres <tournetorres@gmail.com>
2023-12-02 16:00:55 -0600

    add input to modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,12 @@
 import React from "react";
 
+
+const scrollToElement = (elementId) => {
+  const element = document.getElementById(elementId);
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
+};
 const Modal = ({ isOpen, onClose, children }) => {
   if (!isOpen) {
     return null;
@@ -10,8 +17,32 @@ const Modal = ({ isOpen, onClose, children }) => {
       <div className="absolute inset-0 bg-gray-500 opacity-75" onClick={onClose}></div>
       <div className="bg-white p-4 rounded-lg z-20">
         {children}
-        <p className="text-black"> some modal content</p>
-        <button onClick={onClose}>Close Modal</button>
+        <div className="DonationCard max-w-lg  mx-auto rounded-md overflow-hidden  bg-slate-900">
+          <div className="flex items-center p-4 py-6">
+            {/* Circular Avatar Image */}
+            <div className="rounded-full h-12 w-12 flex-shrink-0 bg-white overflow-hidden">
+              <img
+                src="https://nvlupin.blob.core.windows.net/images/van/FAM/FAM/1/87084/images/themes/favicon.png"
+                alt="Avatar"
+                className="object-cover w-full h-full"
+              />
+            </div>
+
+            {/* Content on the Right */}
+            <div className="mx-4 ">
+              <h3 className="bg-scroll-green">Added to Closet!</h3>
+              <p className="text-white">Your sweater is now hanging in the TFG closet and we thank you for contributing meals to Feeding America.</p>
+              <form className="text-white my-4">
+  <label>
+    <input className="w-full py-1 px-2" type="text" name="name" placeholder="Add your name to your work!" />
+  </label>
+  <input className="w-full py-1 px-2 bg-amber-300 hover:bg-amber-400 transition-colors text-slate-800 font-bold cursor-pointer" type="submit" value="Submit" />
+</form>
+
+            </div>
+          </div>
+        </div>
+        {/* <button onClick={onClose}>Close Modal</button> */}
       </div>
     </div>
   );
--- src/modules/ImageTiles.js
+++ src/modules/ImageTiles.js
@@ -54,7 +54,7 @@ const scrollToElement = (elementId) => {
 {/* <div class="flex items-center justify-center h-screen">
   <button class="py-2 px-6 bg-blue-500 text-white rounded hover:bg-blue-700" onclick="toggleModal()">Show Modal</button>
 </div> */}
-{/* <Modal isOpen={showModal} onClose={closeModal}> */}
+<Modal isOpen={showModal} onClose={closeModal}>
 <div class="fixed z-10 overflow-y-auto top-0 w-full left-0 hidden"  id="modal">
   <div class="flex items-center justify-center min-height-100vh pt-4 px-4 pb-20 text-center sm:block sm:p-0">
     <div class="fixed inset-0 transition-opacity">
@@ -74,7 +74,7 @@ const scrollToElement = (elementId) => {
     />
   </div>
   </div>
-  {/* </Modal> */}
+
 
   {/* Content on the Right */}
   <div className="mx-4 ">
@@ -87,7 +87,7 @@ const scrollToElement = (elementId) => {
       </div>
     </div>
   </div>
-</div>
+  </Modal>
     </div>
 
     
