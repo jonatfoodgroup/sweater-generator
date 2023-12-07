67447530f429f6e935195fe9a129318803f401f1
mcasper78 <121960293+mcasper78@users.noreply.github.com>
2023-12-07 12:11:56 -0500

    changing implementation of gtm tag
    added to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,30 @@
+import Head from 'next/head';
 import '../index.css';
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+     <Head>
+     <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              <!-- Google Tag Manager -->
+              (function(w,d,s,l,i) {
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-M5PMNS8');
+              <!-- End Google Tag Manager -->
+            `,
+          }}
+        />
+      </Head> 
+      <Component {...pageProps} />
+      </>
+  );
+  }
+  export default MyApp;
